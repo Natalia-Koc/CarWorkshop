d51791d2dc846a0887e7cbb865e07e9111d10503
Natalia-Koc <koc.natalia249@gmail.com>
2023-09-21 14:27:40 +0200

    ASP.NET Identity

--- src/CarWorkshop.Infrastructure/Persistence/CarWorkshopDbContext.cs
+++ src/CarWorkshop.Infrastructure/Persistence/CarWorkshopDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace CarWorkshop.Infrastructure.Persistence
 {
-    public class CarWorkshopDbContext : DbContext
+    public class CarWorkshopDbContext : IdentityDbContext
     {
         public CarWorkshopDbContext(DbContextOptions<CarWorkshopDbContext> options) : base(options)
         {
@@ -15,12 +16,10 @@ namespace CarWorkshop.Infrastructure.Persistence
 
         public DbSet<Domain.Entities.CarWorkshop> CarWorkshops { get; set; }
 
-        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
-        {
-            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=CarWorkshopDb;Trusted_Connection=True;");
-        }*/
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<Domain.Entities.CarWorkshop>()
                 .OwnsOne(a => a.ContactDetails);
         }
--- src/CarWorkshopMVC/Program.cs
+++ src/CarWorkshopMVC/Program.cs
@@ -36,4 +36,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapRazorPages();
+
 app.Run();
